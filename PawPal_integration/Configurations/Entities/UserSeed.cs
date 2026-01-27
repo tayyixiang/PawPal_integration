8f1e43df401cb9c73f3c04e0ad102b2790f54887
tayyixiang <2403262E@student.tp.edu.sg>
2026-01-28 00:01:38 +0800

    fix adoption application

--- PawPal_integration/Configurations/Entities/UserSeed.cs
+++ PawPal_integration/Configurations/Entities/UserSeed.cs
@@ -13,15 +13,17 @@ namespace PawPal_integration.Configurations.Entities
             builder.HasData(
                 new PawPalUser
                 {
-                    Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                    Id = "3781efa7-66dc-47f0-860f-e506d04102e4",
                     Email = "admin@localhost.com",
                     NormalizedEmail = "ADMIN@LOCALHOST.COM",
-                    UserName = "admin@localhost.com",
-                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
                     FirstName = "Admin",
                     LastName = "User",
-                    EmailConfirmed = true,
-                    PasswordHash = hasher.HashPassword(null, "P@ssword1")
+                    UserName = "admin@localhost.com",
+                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                    PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                    EmailConfirmed = true
+                    // REMOVED properties causing issues if they were inherited from BaseDomainModel
+                    // If you have CreatedBy/UpdatedBy in PawPalUser, set them to "System" instead of null
                 }
             );
         }
